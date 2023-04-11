752fd4c51b34cc84e5e46d7010645b3b0b82014e
changyulin <535441862@qq.com>
2023-04-12 01:53:54 +0800

    密码测试3

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -110,7 +110,8 @@ export const post: APIRoute = async context => {
       }
     }
 
-    const mimakey = ""
+   
+    var mimakey = ""
     if(pwd == password){
       mimakey = "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"
     }else {
